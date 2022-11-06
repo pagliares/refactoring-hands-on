53535ce87f10866606ddf96ba7dc601a69fbe5b3
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2022-11-06 15:12:31 -0300

    Create version 2 extracting method quantiaDe

--- Source_Code/02/refactoring-hands-on/src/main/java/version1/Cliente.java
+++ Source_Code/02/refactoring-hands-on/src/main/java/version1/Cliente.java
@@ -28,25 +28,7 @@ public class Cliente {
             double estaQuantia = 0;
             Locacao cada = (Locacao) locacoes.nextElement();
 
-            // Valor da locacao para cada filme dentro do Vector
-            switch (cada.lerFilme().lerCodigoPreco()) { // codigoPreco associado ao tipo do filme
-                case Filme.NORMAL:
-                    estaQuantia += 2;  // R$ 2,00 por filme normal alugado
-                    if (cada.lerDiasAlugados() > 2) { // Multa de 1.50 por dia de atraso caso seja entregue apos 2 dias
-                        estaQuantia += (cada.lerDiasAlugados() - 2) * 1.5;
-                    }
-                    break;
-                case Filme.LANCAMENTO_NOVO:
-                    estaQuantia += cada.lerDiasAlugados() * 3; // R$ 3,00 a diaria para lancamentos  por filme alugado
-
-                    break;
-                case Filme.INFANTIL:
-                    estaQuantia += 1.5; // R$ 1,50 por 3 dias por filme infantil alugado
-                    if (cada.lerDiasAlugados() > 3) {
-                        estaQuantia += (cada.lerDiasAlugados() - 3) * 1.5; // Multa de 1,50 por dia de atraso caso seja entregue apos 3 dias.
-                    }
-                    break;
-            }             
+            estaQuantia = quantiaDe(cada);
             pontosLocadorFrequente++;
             // Ponto adicional para o locatario para cada filme lancamento
             if ((cada.lerFilme().lerCodigoPreco() == Filme.LANCAMENTO_NOVO)
@@ -65,6 +47,29 @@ public class Cliente {
         resultado += "Pontos adquiridos na locacao : " + String.valueOf(pontosLocadorFrequente);
         return resultado;
 
+    }
+
+    private double quantiaDe(Locacao umaLocacao) {
+        double resultado = 0.0;
+        // Valor da locacao para cada filme dentro do Vector
+        switch (umaLocacao.lerFilme().lerCodigoPreco()) { // codigoPreco associado ao tipo do filme
+            case Filme.NORMAL:
+                resultado += 2;  // R$ 2,00 por filme normal alugado
+                if (umaLocacao.lerDiasAlugados() > 2) { // Multa de 1.50 por dia de atraso caso seja entregue apos 2 dias
+                    resultado += (umaLocacao.lerDiasAlugados() - 2) * 1.5;
+                }
+                break;
+            case Filme.LANCAMENTO_NOVO:
+                resultado += umaLocacao.lerDiasAlugados() * 3; // R$ 3,00 a diaria para lancamentos  por filme alugado
+                break;
+            case Filme.INFANTIL:
+                resultado += 1.5; // R$ 1,50 por 3 dias por filme infantil alugado
+                if (umaLocacao.lerDiasAlugados() > 3) {
+                    resultado += (umaLocacao.lerDiasAlugados() - 3) * 1.5; // Multa de 1,50 por dia de atraso caso seja entregue apos 3 dias.
+                }
+                break;
+        }
+        return resultado;
     }
 }
 
