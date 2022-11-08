1512a230da84ef6ad7e3e90d61622741b5fa2a1c
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2022-11-08 19:18:45 -0300

    Apply Move Method refactoring on method lerPreco

--- Source_Code/09/09_02/refactoring-hands-on/src/main/java/xyz/pagliares/refactoring/entities/Filme.java
+++ Source_Code/09/09_02/refactoring-hands-on/src/main/java/xyz/pagliares/refactoring/entities/Filme.java
@@ -40,26 +40,7 @@ public class Filme {
    }
 
     public double lerPreco(int diasAlugados) {
-        double resultado = 0.0;
-        // Valor da locacao para cada filme dentro do Vector
-        switch (_codigoPreco.lerCodigoPreco()) { // codigoPreco associado ao tipo do filme
-            case Filme.NORMAL:
-                resultado += 2;  // R$ 2,00 por filme normal alugado
-                if (diasAlugados > 2) { // Multa de 1.50 por dia de atraso caso seja entregue apos 2 dias
-                    resultado += (diasAlugados - 2) * 1.5;
-                }
-                break;
-            case Filme.LANCAMENTO_NOVO:
-                resultado += diasAlugados * 3; // R$ 3,00 a diaria para lancamentos  por filme alugado
-                break;
-            case Filme.INFANTIL:
-                resultado += 1.5; // R$ 1,50 por 3 dias por filme infantil alugado
-                if (diasAlugados > 3) {
-                    resultado += (diasAlugados - 3) * 1.5; // Multa de 1,50 por dia de atraso caso seja entregue apos 3 dias.
-                }
-                break;
-        }
-        return resultado;
+       return _codigoPreco.lerPreco(diasAlugados);
     }
 
     public int lerPontosLocadorFrequente(int numDiasAlugado) {
